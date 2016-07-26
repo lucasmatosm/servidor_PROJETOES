160fedeb6d4b190e7529fd74ea388695f9f0cf80
Lucas Matos de Medeiros <lucas.medeiros@ccc.ufcg.edu.br>
2016-07-26 16:59:00 -0300

    Update urls.py

--- projetoes/urls.py
+++ projetoes/urls.py
@@ -17,9 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('povmt.urls')),
+    url(r'^$', RedirectView.as_view(url='povmt/')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
