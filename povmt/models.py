3079c9c5e3120e1c019f8b4f032065310690312f
Lucas Matos de Medeiros <lucas.medeiros@ccc.ufcg.edu.br>
2016-07-29 15:20:28 -0300

    adicionando data de criação

--- povmt/models.py
+++ povmt/models.py
@@ -5,7 +5,11 @@ from povmt.utils import download_path
 class Usuario(models.Model):
     nome = models.CharField(max_length=100, blank=True, default='')
     email = models.CharField(max_length=100, blank=True, default='')
+    data_created = models.DateTimeField(auto_now_add=True)
     url_foto  = models.CharField(max_length=100, blank=True, default='')
+
+    def __unicode__(self):
+        return  str(self.nome)
 
 
 class Atividade(models.Model):
@@ -13,6 +17,7 @@ class Atividade(models.Model):
     categoria = models.CharField(max_length=100, blank=True, default='')
     prioridade = models.CharField(max_length=100, blank=True, default='')
     url_imagem = models.ImageField(upload_to=download_path, default="media/No-img.jpg", null=True, blank=True)
+    data_created = models.DateTimeField(auto_now_add=True)
     id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column='id_usuario')
 
     def __unicode__(self):
@@ -23,8 +28,10 @@ class Tinvestido(models.Model):
     data = models.CharField(max_length=100, blank=True, default='')
     semana = models.IntegerField()
     horas = models.IntegerField()
+    data_created = models.DateTimeField(auto_now_add=True)
     id_atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE, db_column='id_atividade')
 
 class Tag(models.Model):
     nome = models.CharField(max_length=100, blank=True, default='')
     id_atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE, db_column='id_atividade')
+    data_created = models.DateTimeField(auto_now_add=True)
