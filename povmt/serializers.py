08417e9237dd07ab151bcdeddc15ae56f867daac
Lucas Matos de Medeiros <lucas.medeiros@ccc.ufcg.edu.br>
2016-08-01 20:35:42 -0300

    Update serializers.py

--- povmt/serializers.py
+++ povmt/serializers.py
@@ -9,7 +9,7 @@ from povmt.models import Tinvestido
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ('nome', 'email', 'url_foto', 'id')
+        fields = ('nome', 'email', 'url_foto', 'id','data_created')
 
 class UsuarioSerializerID(serializers.ModelSerializer):
     class Meta:
@@ -20,7 +20,7 @@ class UsuarioSerializerID(serializers.ModelSerializer):
 class TISerializer(serializers.ModelSerializer):
     class Meta:
         model = Tinvestido
-        fields = ('data', 'semana', 'horas', 'id_atividade','id')
+        fields = ('data', 'semana', 'horas', 'id_atividade','id','data_created')
 
 class TISerializerID(serializers.ModelSerializer):
     class Meta:
@@ -30,19 +30,19 @@ class TISerializerID(serializers.ModelSerializer):
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ('nome', 'id_atividade','id')
+        fields = ('nome', 'id_atividade','id','data_created')
 
 class TagSerializerID(serializers.ModelSerializer):
     class Meta:
         model = Tag
 
 
-        
+
 class AtividadeSerializer(serializers.ModelSerializer):
     url_imagem = serializers.ImageField(max_length=None, required=False, use_url=True)
     class Meta:
         model = Atividade
-        fields = ('nome', 'categoria', 'prioridade', 'url_imagem', 'id_usuario', 'id')
+        fields = ('nome', 'categoria', 'prioridade', 'url_imagem', 'id_usuario', 'id','data_created')
 
 class AtividadeSerializerID(serializers.ModelSerializer):
     class Meta:
