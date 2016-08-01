317024abc2c781800283a1aa00c07dcb5cdb5fe9
Lucas Matos de Medeiros <lucas.medeiros@ccc.ufcg.edu.br>
2016-08-01 20:39:01 -0300

    Update urls.py

--- povmt/urls.py
+++ povmt/urls.py
@@ -3,6 +3,7 @@ from povmt import views
 
 urlpatterns = [
     url(r'^povmt/$', views.cria_usuario), #criar usuario
+    url(r'^povmt/user/(?P<id_usuario>\d+)$', views.get_usuario), #recupera usuario pelo id
     url(r'^povmt/(?P<id_usuario>\d+)/$', views.criaAtividade), #atividades daquele usuario
     url(r'^povmt/atividade/(?P<id>\d+)/$', views.getAtividade), #pega uma atividade especifica
     url(r'^povmt/tilist/(?P<id_atividade>\d+)/$', views.criaTI), #lista e cria TI para um atividade
